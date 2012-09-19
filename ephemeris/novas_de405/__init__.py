589f2be574afde376e0dbe2e3553d2931e098542
Brandon Rhodes <brandon@rhodesmill.org>
2012-09-19 19:01:19 -0400

    Created a "novas_de405" ephemeris package

---
+++ ephemeris/novas_de405/__init__.py
@@ -0,0 +1,1 @@
+"""The JPL DE405 ephemeris binary data file for the `novas` package."""
---
+++ ephemeris/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+long_description = open('README').read().decode('utf-8')
+
+setup(name='novas_de405',
+      version='1997',
+      description=u'JPL DE405 ephemeris needed by the NOVAS package',
+      long_description=long_description,
+      maintainer='Brandon Rhodes',
+      maintainer_email='brandon@rhodesmill.org',
+      url='http://jplephem.s3.amazonaws.com/packages.html',
+      packages=['novas_de405'],
+      package_data={'novas_de405': ['*.bin']},
+      )
