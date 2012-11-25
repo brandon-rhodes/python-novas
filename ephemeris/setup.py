6f527cbac8917f9dd1eb4b3fda7d574475e8bf4f
Brandon Rhodes <brandon@rhodesmill.org>
2012-11-25 18:54:03 -0500

    Fix the novas-de405 "setup.py" to work on Python 3

--- ephemeris/setup.py
+++ ephemeris/setup.py
@@ -1,10 +1,10 @@
 from distutils.core import setup
 
-long_description = open('README').read().decode('utf-8')
+long_description = open('README', 'rb').read().decode('utf-8')
 
 setup(name='novas_de405',
       version='1997',
-      description=u'JPL DE405 ephemeris needed by the NOVAS package',
+      description='JPL DE405 ephemeris needed by the NOVAS package',
       long_description=long_description,
       maintainer='Brandon Rhodes',
       maintainer_email='brandon@rhodesmill.org',
