37516f8285e3c6604872bf5b674edba7054a0c04
Brandon Rhodes <brandon@rhodesmill.org>
2013-07-31 14:55:13 -0400

    Try loading a wider variety of shared-object names
    The "Python 3 compatible" code that I was using before is now, alas,
    failing on Python 3.3 on my Ubuntu 13.04 laptop, because the new-style
    shared object name, that I was looking for, is now back to being a
    normal ".so" file.  So, with no way to guess what the right name is, I
    am adding a loop over several possible names to try to select the right
    one.

--- novas_py/__init__.py
+++ novas_py/__init__.py
@@ -5,26 +5,24 @@ import sys
 from ctypes import CDLL
 
 # By Brandon Rhodes, for Python 3 compatibility:
-if sys.version_info >= (3, 2):
-	import sysconfig
-	soabi = sysconfig.get_config_var('SOABI')
-	libname = 'libnovas.{}.so'.format(soabi)
-else:
-	libname = 'libnovas.so'
+def load_shared_library():
+    global novaslib
 
-for directory in sys.path:
-	try:
-		novaslib = CDLL(os.path.join(directory, 'novas', libname))
-		break
-	except:
-		# Also by Brandon Rhodes, for platforms where the SOABI
-		# under Python 3 has a different value than the one used
-		# to name the .so
-		if libname.endswith('-33dm.so'):
-			altname = libname.replace('-33dm.so', '-33m.so')
-			try:
-				novaslib = CDLL(os.path.join(
-                                                directory, 'novas', altname))
-				break
-			except:
-				pass
+    names = ['libnovas.so']
+    if sys.version_info >= (3, 2):
+        import sysconfig
+        name = 'libnovas.{}.so'.format(sysconfig.get_config_var('SOABI'))
+        names.append(name)
+        if name.endswith('-33dm.so'):
+            names.append(name.replace('-33dm.so', '-33m.so'))
+
+    for directory in sys.path:
+        for name in names:
+            try:
+                novaslib = CDLL(os.path.join(directory, 'novas', name))
+                break
+            except:
+                pass
+
+load_shared_library()
+del load_shared_library
