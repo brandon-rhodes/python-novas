0f01459cd31665a8176a67b575c548360ff4ae86
Brandon Rhodes <brandon@rhodesmill.org>
2014-06-11 12:40:38 -0400

    Give ephemeris a real version number: "1997.1"
    The Python "pip" install tool no longer likes version numbers without a
    dot in the name, so this adds one.  Also, this changes the project URL
    to the GitHub repository, since I am now hosting the package on PyPI
    instead of at the old external URL (which pip no longer likes) over on
    my S3 bucket.

--- ephemeris/setup.py
+++ ephemeris/setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 long_description = open('README', 'rb').read().decode('utf-8')
 
 setup(name='novas_de405',
-      version='1997',
+      version='1997.1',
       description='JPL DE405 ephemeris needed by the NOVAS package',
       long_description=long_description,
       maintainer='Brandon Rhodes',
       maintainer_email='brandon@rhodesmill.org',
-      url='http://jplephem.s3.amazonaws.com/packages.html',
+      url='https://github.com/brandon-rhodes/python-novas',
       packages=['novas_de405'],
       package_data={'novas_de405': ['*.bin']},
       )
